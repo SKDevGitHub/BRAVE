2ec39963bbd581c73ff1646f1b0bb8583a689f19
Eliot <eaknxp@umsystem.edu>
2026-08-17 18:31:32 +0000

    training script

--- train.sh
+++ train.sh
@@ -1,3 +1,4 @@
+#!/usr/bin/bash
 source .venv/bin/activate
-WANDB_MODE=disabled uv run train Mjlab-LocoManip-BRAVE-G1 --num-gpus=all
+WANDB_MODE=disabled uv run train Mjlab-LocoManip-BRAVE-G1 --gpu-ids all
 
